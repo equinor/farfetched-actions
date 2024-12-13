25ff3ec78f5014ba134545a064b467a8172d0a48
Jan-Vidar Ølberg <156783199+janvidaro@users.noreply.github.com>
2024-12-13 10:00:12 +0100

    chore: wip

--- update-yarn/update-yarn.sh
+++ update-yarn/update-yarn.sh
@@ -1,25 +1,8 @@
 #!/bin/bash
 set -e
 
-RED='\033[0;31m'
 LIGHT_CYAN='\033[0;36m'
 RESET='\033[0m' # No Color
-
-LABEL_NAME="yarn-update"
-
-# Create label if it doesn't exist
-if ! gh label list | grep --quiet "$LABEL_NAME"; then
-  gh label create "$LABEL_NAME" --color "ededed" --description "A pull request created to update the Yarn package manager version."
-fi
-
-# Ensure pull request doesn't exist
-EXISTING_YARN_PULL_REQUESTS=$(gh pr list --label "$LABEL_NAME" --state open)
-if [ -z "$EXISTING_YARN_PULL_REQUESTS" ]; then
-  echo "No existing Yarn update PR found."
-else
-  echo "Existing Yarn update PR found. Skipping update."
-  exit 0;
-fi 
 
 # Update yarn if not already updated
 PREVIOUS_YARN_VERSION=$(jq -r '.packageManager' package.json | grep -oP "yarn@\K.*")
